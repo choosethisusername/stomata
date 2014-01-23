924eea4e2787ed02fe5e7b746642046e314fa525
Akshay Karle <akshay.a.karle@gmail.com>
2014-01-23 10:28:12 -0300

    Fail a test

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class PostTest < ActiveSupport::TestCase
 
   test "create a post" do
+    assert false
     p = Post.create(:content => 'trivial', :title => "foo")
     assert_equal 1, Post.count
     assert_equal 'trivial', p.content
